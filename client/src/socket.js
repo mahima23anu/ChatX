d40fcea7aeb948b69835ee28e0d8c8a3f29cfdae
Aditi Srivastava <aditisrivastava7788@gmail.com>
2023-06-01 23:54:01 +0530

    commit2

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,3 @@
-// import io from 'socket.io-client';
-
-// const socket = io('http://localhost:3000');
-
 
 import Prompt from './components/Prompt';
 // import  { useState, useEffect } from 'react';
