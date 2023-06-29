189cf7da36ab8dfdb6405f73af76e253c067afb3
Aditi Srivastava <aditisrivastava7788@gmail.com>
2023-06-29 12:14:33 +0530

    socket code at client and server side added

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,8 @@ import Prompt from './components/Prompt';
 // import  { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000'); // Replace with your server URL
+// const socket = io('http://localhost:8000'); 
+// console.log("Inside socket.js")
 
 function MyComponent() {
   const [name, setName] = useState('');
